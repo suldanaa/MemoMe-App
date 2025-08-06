3e003dc037b62d10080f42a6b9ccd236714c0766
Suldana Abdulkadir <sabdulkadirafrah@ucsd.edu>
2025-08-05 21:40:27 -0700

    Initial Commit

---
+++ MemoMeTests/MemoMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoMeTests.swift
+//  MemoMeTests
+//
+//  Created by Suldana Afrah on 8/5/25.
+//
+
+import Testing
+@testable import MemoMe
+
+struct MemoMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
